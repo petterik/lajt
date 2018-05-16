011fd7a471e881f81d9386b01d18b010e32b2d75
Petter Eriksson <ipetterik@gmail.com>
2018-05-16 15:57:33 +0200

    Adds a project.clj to be able to push to clojars.
    1. I need all deps in petterik/lajter to be :mvn/version
       deps for "clj -Spom" to work, which is needed for cursive
       to work with deps.edn.
    2. I couldn't get pushing to clojars with mvn to work.
    
    Hacks all the way down.

---
+++ project.clj
@@ -0,0 +1,19 @@
+(defproject org.clojars.petterik/lajt "0.1.1-SNAPSHOT"
+  :repositories [["clojars" {:sign-relases false}]]
+  :dependencies 
+  [[org.clojure/clojure "1.10.0-alpha4" :scope "provided"]
+   [org.clojure/clojurescript "1.10.217" :scope "provided"]
+   [datascript "0.16.5" :scope "provided"]
+   [com.stuartsierra/dependency "0.2.0"]
+   [medley "1.0.0"]]
+
+  :profiles
+  {:dev {:dependencies
+         [[org.clojure/test.check "0.10.0-alpha2"]
+          [com.clojure-goes-fast/clj-memory-meter "0.1.0"]
+          [expound "0.5.0"]]}}
+
+  :source-paths ["src"]
+  :test-paths ["test"]
+)
+                  
